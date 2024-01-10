afa0e6c0a472179bca7274d4f9b23a4d52d1133c
richie <teorichiemalaya@gmail.com>
2024-01-11 02:26:30 +0800

    added comments for app js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,21 @@ import Account from "./pages/Account";
 import { useEffect, useState } from "react";
 
 function App() {
+  // Log the supabase object to the console
   console.log(supabase);
+
+  // State to manage the user token
   const [token, setToken] = useState(false);
+
+  // Save the token to sessionStorage if it exists
   if (token) {
     sessionStorage.setItem("token", JSON.stringify(token));
   }
 
+  // Check sessionStorage for an existing token on initial load
   useEffect(() => {
     if (sessionStorage.getItem("token")) {
+      // Parse the token and set it to the state
       let data = JSON.parse(sessionStorage.getItem("token"));
       setToken(data);
     }
@@ -26,6 +33,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        {/* Define routes for different pages */}
         <Routes>
           <Route path="/" element={<Login setToken={setToken} />} />
           <Route path="/register" element={<Register />} />
